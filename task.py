7cfe58438c78614bab8d381b3a7b86926b05250b
salina <salinaadhikari606@gmail.com>
2024-12-18 16:42:46 +0545

    new file

---
+++ task.py
@@ -0,0 +1,19 @@
+"""my_list =[1,2,3,"a","b","c"]
+my_list.append("d")
+
+my_list.remove(2)
+print(len(my_list))
+print(my_list)"""
+
+"""my_tuple=(10, 20, 30, 40, 50)
+print(my_tuple[2])
+num=list(my_tuple)
+num[3]=35
+typle_1=tuple(num)
+print(typle_1)"""
+
+name=input("enter your name")
+print(len(name))
+n_1="he$oo is my name is $salina $dhikari"
+count_dollar = n_1.count("$")
+print(count_dollar)
---
+++ threshold.py
@@ -0,0 +1,70 @@
+"""batteries=[50,30,4,45,12,18,30]
+mimimal=20
+usable=0
+count=0
+for battery in batteries:
+    if battery > mimimal:
+        usable += battery
+        count = count + 1
+
+print(usable,count)
+
+
+
+alien="uoy erutpac anoog era ew namuh ih"
+print(alien)
+human=alien[::-1]
+print(human)
+
+available=[
+ "apple",
+ "banana",
+ "kiwi",
+ "kit kat",
+ "grapes",
+ "water",
+ "berry",
+]
+crew=4
+each=len(available) // crew
+remaining=len(available) % crew
+print("each crew gets:",{each}," and remaining:",{remaining})
+
+"""
+
+def setup_mission():
+    print("setting up the mission")
+    available=[
+ "apple",
+ "banana",
+ "kiwi",
+ "kit kat",
+ "grapes",
+ "water",
+ "berry",
+]
+    crew_available=int(input("enter the num of crew"))
+    print("setup completed")
+    return crew_available,available
+    
+    
+
+def freedom_game():
+    print("Weelcome to the world of Survival")
+    print("mission started............")
+
+    crew_number,food=setup_mission()
+    print(f"you have= {crew_number}","and number of foods ={food}")
+    
+    print("welcome to mars!!!!")
+    print("your battery is dead plese charge")
+    print("mission completed")
+
+
+
+freedom_game()
+
+
+
+   
+
